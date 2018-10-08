987a724f7c1b5251d88396aef7d38af29e8abc43
Doug Beney <contact@dougie.io>
2018-10-07 23:04:07 -0400

    Fixed appimage script

--- make-appimage.sh
+++ make-appimage.sh
@@ -4,14 +4,15 @@ rm -rf dist/*
 cd dist
 
 echo Creating ./dist/Makefile using qmake.....
-qmake ../WideGuy.pro -o Makefile
+qmake ../WideGuy.pro -o Makefile || { echo 'Failed to run qmake. Is it in your path?' ; exit 1; }
 
 
 echo Compiling application...
-make
+make || { echo 'Failed to compile project' ; exit 1; }
 
 echo Moving over ../usr folder...
 cp -r ../usr .
+mkdir usr/bin
 cp WideGuy usr/bin
 
 echo Downloading LinuxDeployQt
@@ -21,9 +22,9 @@ echo Asking for permission to run command "chmod +x $PWD/linuxdeployqt-continuou
 pkexec chmod +x $PWD/linuxdeployqt-continuous-x86_64.AppImage
 
 echo Creating AppImage...
-./linuxdeployqt-continuous-x86_64.AppImage usr/share/applications/wideguy.desktop -appimage
+./linuxdeployqt-continuous-x86_64.AppImage usr/share/applications/wideguy.desktop -appimage || { echo 'Failed building AppImage' ; exit 1; }
 
 echo
 echo ===========================
-echo DONE!!
+echo Great success!!
 echo AppImage is located in $PWD
